e0cfdb26184719147ef9149c2f305257b80d4ed5
Lady30-19 <laura.apaza1430@gmail.com>
2023-11-08 19:40:00 -0500

    examen

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      
+      <h1>Hola soy Lady Laura Quispe Apaza </h1>
+      <h3>Fecha: 08/11/2023 / Turno: noche</h3>
+      <h3>Carrera: Computación Informática</h3>
+      
     </div>
   );
 }
